b559c3f8d7ac433ca7e61da4cbe3728c4523c3db
rkrukshan <rukshan1122@gmail.com>
2025-09-23 13:11:30 +0530

    Quotes Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Navbar } from './components/Navbar/Navbar'
 import { Hero } from './components/Hero/Hero'
+import { Quotes } from './Quotes'
 
 export default function App() {
   return (
-    <main className='overflow-x-hidden bg-white dark:bg-black'>
-      <Navbar/>
-      <Hero/>
+    <main className='overflow-x-hidden bg-white dark:bg-black text-black dark:text-white'>
+      <Navbar />
+      <Hero />
+      <Quotes/>
     </main>
   )
 }
---
+++ src/Quotes.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Quotes = () => {
+  return (
+    <>
+    <div className="container py-6 px-4">
+        <h1 className="text-2xl sm:text-4xl font-bold max-w-[650px] mx-auto text-center font-parisienne">
+            "Virtual reality is the first step in a grand adventure into the landscape of the imagination." - Frank Biocca
+        </h1>
+    </div>
+    </>
+  )
+}
